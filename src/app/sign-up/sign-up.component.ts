464ddd823e8765ddad987fb24325e5fef466a521
Farah SELTANY <farahseltany@gmail.com>
2019-10-08 13:34:02 +0200

    User profile

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class SignUpComponent implements OnInit {
 
   email : string;
-  constructor() { }
+  constructor() { 
+    this.email;
+  }
 
   ngOnInit() {
   }
